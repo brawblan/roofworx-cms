ecd8f989b65615eb20436d28bce89c85302a3d31
brandonblankenstein <brandon.blankenstein@gmail.com>
2022-04-26 23:08:40 -0500

    no more pets

---
+++ schemas/company.js
@@ -0,0 +1,37 @@
+export default {
+  name: 'company',
+  type: 'document',
+	title: 'Company',
+  fields: [
+    {
+      name: 'header',
+      type: 'string',
+      title: 'Header'
+    },
+    {
+      name: 'header_description',
+      type: 'string',
+      title: 'Header Description'
+    },
+    {
+      name: 'content_one_header',
+      type: 'string',
+      title: 'Content One Header'
+    },
+    {
+      name: 'content_one_description',
+      type: 'string',
+      title: 'Content One Description'
+    },
+    {
+      name: 'content_two_header',
+      type: 'string',
+      title: 'Content Two Header'
+    },
+    {
+      name: 'content_two_description',
+      type: 'string',
+      title: 'Content Two Description'
+    },
+  ]
+}
--- schemas/schema.js
+++ schemas/schema.js
@@ -1,10 +1,10 @@
 import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-import pet from './pet'
+import company from './company'
 
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
-    pet,
+    company
   ]),
 })
